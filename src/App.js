ccdab5f4a88a1658124a8753782393b26b9da825
HARISH04SS <harishsubramanian4122001@gmail.com>
2024-06-22 03:27:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const plans = [
+  {
+    name: "FREE",
+    price: "$0/month",
+    features: [
+      "✔ Single User",
+      "✔ 50GB Storage",
+      "✔ Unlimited Public Projects",
+      "✔ Community Access",
+      <span style={{opacity:0.5}}>x Unlimited Private Projects</span>,
+      <span style={{opacity:0.5}}>x Dedicated Phone Support</span>,
+      <span style={{opacity:0.5}}>x Free Subdomain</span>,
+      <span style={{opacity:0.5}}>x Monthly Status Reports</span>,
+    ],
+  },
+  {
+    name: "PLUS",
+    price: "$9/month",
+    features: [
+      "✔ 5 Users",
+      "✔ 50GB Storage",
+      "✔ Unlimited Public Projects",
+      "✔ Community Access",
+      "✔ Unlimited Private Projects",
+      "✔ Dedicated Phone Support",
+      "✔ Free Subdomain",
+      <span style={{opacity:0.5}}>x Monthly Status Reports</span>,
+    ],
+  },
+  {
+    name: "PRO",
+    price: "$49/month",
+    features: [
+      "✔ Unlimited Users",
+      "✔ 50GB Storage",
+      "✔ Unlimited Public Projects",
+      "✔ Community Access",
+      "✔ Unlimited Private Projects",
+      "✔ Dedicated Phone Support",
+      "✔ Free Subdomain",
+      "✔ Monthly Status Reports",
+    ],
+  },
+];
+
+const PlanCard = ({ name, price, features }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className='sp'style={{opacity:0.5}}> {name}</div>
+      <h1>{price}</h1>
+      <ul>
+        {features.map((feature, index) => (
+          <li key={index}>{feature}</li>
+        ))}
+      </ul>
+      <button className="button">BUTTON</button>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="plan-container">
+        {plans.map((plan, index) => (
+          <PlanCard key={index} {...plan} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
